d1df9951db5d9b0fd5ee1f0b85f6bf8f8524bed7
kosakarin <91523573+kosakarin@users.noreply.github.com>
2021-11-08 14:46:08 +0800

    完善控分计算器计算算法

--- countpoint.py
+++ countpoint.py
@@ -1,42 +1,38 @@
 
-
+#默认
 songs = [
-    875,  #20LP S 
-    860,  #20LP A
-    640,  #16LP S
-    620,  #16LP Aoi
-    600,  #15LP S
-    581,  #15LP A
-    438,  #13LP S
-    422,  #13LP A
-    405,  #12LP S
-    390,  #12LP A
-    275,  #10LP S 
-    262,  #10LP A 
-    247,  #9LP S
-    236   #9LP A
+    875, 640, 600, 438, 405, 275,  247,  #S
+    860, 620, 581, 422, 390, 262,  236   #A
 ]
-
-
 songs_list = [
-    '上+ S',
-    '上+ A',
-    '16LP剧情关 S',
-    '16LP剧情关 A',
-    '上级 S',
-    '上级 A',
-    '13LP剧情关 S',
-    '13LP剧情关 A',
-    '中级 S',
-    '中级 A',
-    '初级 S',
-    '初级 A',
-    '9LP剧情关 S',
-    '9LP剧情关 A'
-
+    '上+ S', '16LP剧情关 S', '上级 S', '13LP剧情关 S', '中级 S', '初级 S', '9LP剧情关 S',
+    '上+ A', '16LP剧情关 A', '上级 A', '13LP剧情关 A', '中级 A', '初级 A', '9LP剧情关 A'
+]
+#无剧情关
+songs_normal = [
+    875, 600, 405, 275,  #S
+    860, 581, 390, 262   #A
+]
+songs_list_normal = [
+    '上+ S', '上级 S', '中级 S', '初级 S',
+    '上+ A', '上级 A', '中级 A', '初级 A'
+]
+#无A
+songs_S = [
+    875, 640, 600, 438, 405, 275,  247  #S
+]
+songs_list_S = [
+    '上+ S', '16LP剧情关 S', '上级 S', '13LP剧情关 S', '中级 S', '初级 S', '9LP剧情关 S'
+]
+#无剧情关且全S
+songs_normal_S = [
+    875, 600, 405, 275
+]
+songs_list_normal_S = [
+    '上+ S', '上级 S', '中级 S', '初级 S'
 ]
 
-def solve(songs,pt:int):
+def solve(songs, pt:int):
     #print(pt)
     res = []
     visited = [False] * (pt + 10)
@@ -53,37 +49,46 @@ def solve(songs,pt:int):
             visited[j + i]=True
             for k in mp[j]:
                 mp[j + i][k]=mp[j][k]
-            if mp[j + i].get(i)==None:
-                mp[j + i][i]=1
+            if mp[j + i].get(i) == None:
+                mp[j + i][i] = 1
             else:
-                mp[j + i][i]+=1
+                mp[j + i][i] += 1
     return mp[pt]
 
-def tran(i:int) -> str:
+def chose_list(mod:int):
+    if mod == 1:
+        return songs_S, songs_list_S, '当前为仅S模式'
+    elif mod == 2:
+        return songs_normal, songs_list_normal, '当前为无剧情关卡模式'
+    elif mod == 3:
+        return songs_normal_S, songs_list_normal_S, '当前为无剧情关卡仅S模式'
+    else:
+        return songs, songs_list, '未提供模式参数或模式参数识别失败，使用默认模式'
+
+def tran(_songs, _songs_list, i:int) -> str:
     temp = 0
-    for song in songs:
+    for song in _songs:
         if i == song:
-            return songs_list[temp]
+            return _songs_list[temp]
         temp += 1
 
-def count_point(target:int, x:int) -> str:
+def count_point(target:int, x:int, mod) -> str:
     target -= x
     p = target
     cnt = 0
     msg = ''
+    _songs, _songs_list, mod_str = chose_list(mod)
     if target >= 50000:
         cnt = (target - 49000)//875
         target -= cnt * 875
-    res = solve(songs, target)
-    msg += f'还需要获得{str(p)}pt,我们采用以下方案：\n注:以下结果为0%加成结果\n'
+    res = solve(_songs, target)
+    msg += f'还需要获得{str(p)}pt,我们采用以下方案：\n注:以下结果为0%加成结果\n{mod_str}\n'
     if len(res) == 0:
         msg += '无解'
         return msg
     for i in res:
         if i == 875:
             res[i] += cnt
-        msg += f'\n游玩{res[i]}次'
-        msg += tran(i)
-        msg += f'，每次获得{str(i)}pt，共获得{str(i*res[i])}pt'
-        
+        msg += f'\n游玩{res[i]}次{tran(_songs, _songs_list, i)}，每次获得{str(i)}pt，共获得{str(i * res[i])}pt'
+  
     return msg
