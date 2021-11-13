4b83764f665ed28d882a7f829f7238e5674e3804
kosakarin <91523573+kosakarin@users.noreply.github.com>
2021-11-13 13:38:36 +0800

    补充了下注释

--- __init__.py
+++ __init__.py
@@ -4,7 +4,7 @@ from .asgacha import *
 from .image import *
 from .money import *
 from .countpoint import count_point
-from .config import get_config, get_group_config, set_group_config
+from .config import get_config, get_group_config, set_group_config  
 from .card_save import *
 from hoshino import Service, R
 from hoshino.typing import CQEvent
@@ -35,31 +35,31 @@ sv = Service('as抽卡', bundle='as', help_='''使用如下命令进行抽卡模
 
 base_path = 'C:\/Users/Administrator/Desktop/XCW/res/img/image/frame.png'         #底图路径
 icon_path = 'C:\/Users/Administrator/Desktop/XCW/res/img/'                        #icon路径
-COL_NUM = 8
-cid_list = {'1': [], '2': [], '3': []}
-cids_list = []
+COL_NUM = 8  #生成仓库图片时一行显示几张（我自己只试过8张，别的不清楚会不会错位啥的）
+cid_list = {'1': [], '2': [], '3': []}  #将各个等级下的卡片id存进来
+cids_list = [] #用来存当前所有的卡片id
 
 def load_cards(): #预加载所有卡片资源
     global cids_list
     global cid_list
-    for key in cid_list.keys():
-        for i in range(1,4):
-            for ii in range(1,13):
-                for iii in range(1,15):
-                    a = str(ii) if ii > 9 else str(f"0{ii}")
-                    b = str(iii) if  iii > 9 else str(f"0{iii}")
-                    rare = 'ur' if key == '3' else ('sr' if key == '2' else 'r')
-                    _path = f"{icon_path}image/icon_{rare}/{i}/{ii}/{iii}.png"
-                    cid = int(f"{key}{i}{a}{b}1")
+    for key in cid_list.keys():  #[稀有度]: 1 r; 2 sr; 3 ur;         #我自己根据[稀有度][团体编号][角色编号][卡片编号][觉醒标记]的规则
+        for i in range(1,4):     #[团队编号]: 1 us; 2 aqours; 3 虹；  #生成了一个7位int型cid，可以调用asgacha.py下的get_cid([稀有度], 
+            for ii in range(1,13):  #[角色编号]: 具体见help文本中的顺序 # [团体编号],[角色编号],[卡片编号])来获取一张未觉醒的卡片cid（因
+                for iii in range(1,15): #[卡片编号]：随便编的          #为目前用不到觉醒卡片所以结尾默认写1了 
+                    a = str(ii) if ii > 9 else str(f"0{ii}") #自动补0
+                    b = str(iii) if  iii > 9 else str(f"0{iii}") #自动补0
+                    rare = 'ur' if key == '3' else ('sr' if key == '2' else 'r') #转化稀有度为数字
+                    _path = f"{icon_path}image/icon_{rare}/{i}/{ii}/{iii}.png"  #转化为对应路径
+                    cid = int(f"{key}{i}{a}{b}1")  #转化为cid
                     try:
-                        Image.open(_path)
-                        cid_list[key].append(cid)
+                        Image.open(_path)  #利用open来尝试是否存在此卡片
+                        cid_list[key].append(cid) #存在则分别存入两个list
                         cids_list.append(cid)
                     except:
-                        continue                    
-load_cards()
+                        continue   #错误，则不存在，继续循环
+load_cards() #载入时缓存所有cid
 
-config_list = [        
+config_list = [         #用于检查设置时关键字是否正确
     "up_num",
     "up_card1",
     "up_card2",
@@ -70,75 +70,67 @@ config_list = [
     "gacha_cost"
 ]
 
-def normalize_digit_format(n):
+def normalize_digit_format(n): #int转str自动补0
     return f'0{n}' if n < 10 else f'{n}'
 
-def get_pic(pic_path:str, row:int):  #自动访问路径获取icon返回(返回的img像素为80*80)
+def get_pic(pic_path:str, row:int):  #自动访问路径获取icon返回(row控制返回的图片大小)
     _path = icon_path + pic_path
     img = Image.open(_path)
     img = img.resize((row, row), Image.ANTIALIAS)
     return img
 
     
-def get_pic_ex(pic_path:str, num, row:int):  #自动访问路径获取icon返回(同时返回卡片破数贴图)
+def get_pic_ex(pic_path:str, num, row:int):  #自动访问路径获取icon返回(根据卡片数量返回贴了破数贴图的图片)
     _path = icon_path + pic_path
     img = Image.open(_path)
     img = img.resize((row, row), Image.ANTIALIAS)
     star = get_pic("image/star.png", 16)
     star_disabled = get_pic("image/star_disabled.png", 16)
-    num -= 1
-    if num > 0:
+    num -= 1  #我定义num==2时是1破（1宝） ==3时2破 以此类推
+    if num > 0: #如果num == 1 在上面-=1后就不会开始贴破数
         for i in range(0, 5):
             if i < num:
-                img.paste(star, (i * 16, 60), star)
+                img.paste(star, (i * 16, 60), star) #我自己内部写的是调用这个函数的row都是80像素 这里直接/5填了16，可以根据实际情况修改
             elif i >= num:
                 img.paste(star_disabled, (i * 16, 60), star_disabled)
     return img
 
-def get_grey_pic(pic_path, row):
+def get_grey_pic(pic_path, row): #返回一张黑白的图片（灰度图片）
     _path = icon_path + pic_path
     img = Image.open(_path)
     img = img.resize((row, row), Image.ANTIALIAS)
     img = img.convert('L')
     return img  
     
-@sv.on_prefix('gacha')
-async def send_gacha(bot, ev):
-    uid = ev['user_id']
-    gid = ev['group_id']
-    is_su = hoshino.priv.check_priv(ev, hoshino.priv.SUPERUSER)
+@sv.on_prefix('gacha')  
+async def send_gacha(bot, ev): #用于不重启bot设置一些东西，应该可以完善一下，比如不要一条一条设置之类的，以后再改吧
+    uid = ev['user_id']   #获取触发者的uid：int
+    is_su = hoshino.priv.check_priv(ev, hoshino.priv.SUPERUSER) #判断权限是否足够
     args = ev.message.extract_plain_text().split()
 
     msg = ''
-    if not is_su:
+    if not is_su:  #权限不足直接退出
         await bot.send(ev, '需要超级用户权限')
         return
     elif len(args) == 0:
         await bot.send(ev, '输入错误')
         return
-    elif args[0] == 'set' and len(args) >= 3: #setu set module on [group]
-        if len(args) >= 4 and args[3].isdigit():
-            gid = int(args[3])
-
+    elif args[0] == 'set' and len(args) >= 3: #识别到是set关键字，检测下一个关键字
         key = args[1]
-        for i in config_list:
+        for i in config_list: #检查关键字合法性
             if key == i:
                 break
         else:
             await bot.send(ev, '无此项参数')
             return
-        value = False
         string = args[2]
         temp = 0
-
-        if args[2].isdigit():
+        if args[2].isdigit():  #这里主要是为了将数字型的参数以int存进去，现在看看直接try except会好一些
             value = int(args[2])
             temp = 1
         else:
             temp = 2
-            
         if temp == 1:
-            
             try:
                 set_group_config(key, value)
                 msg = str(value)
@@ -154,10 +146,7 @@ async def send_gacha(bot, ev):
                 return
         await bot.send(ev, msg)
         return
-
-    elif args[0] == 'get':
-        if len(args) >= 2 and args[1].isdigit():
-            gid = int(args[1])
+    elif args[0] == 'get': #识别到关键字是get（asgacha get），返回当前参数列表
         msg += f'up_num : {get_group_config("up_num")}'
         msg += f'\nup_card1 : {get_group_config("up_card1")}'
         msg += f'\nup_card2 : {get_group_config("up_card2")}'
@@ -176,56 +165,55 @@ async def send_gacha(bot, ev):
 @sv.on_rex((r'^(as|as )(.*)(一抽|单抽)(.*)'))                              #我讨厌正则表达式
 async def gacha_one(bot, ev):  #单抽
     uid = ev['user_id']
-    key = "starstone"
-    now_starstone = get_user_money(uid, key)
-    kirastone_num = 0
+    key = "starstone" #多少有点多此一举了
+    now_starstone = get_user_money(uid, key)   #获得触发者当前有多少星石  在money.py下（凡是涉及到金钱操作的都在那里面
+    kirastone_num = 0 #提前声明变量
     try:
         arg = str.strip(ev['match'].group(4))
-        member, team = change_name(arg) 
+        member, team = change_name(arg) #这个是asgacha.py内的一个名称转化函数，可以将识别到的关键字转成对应的团队编号和角色编号，栞的识别有点问题 需要注意
     except:
         member = 0
         team = 0
     if member == 11 or member == 12:
-        await bot.finish(ev, '暂不支持米娅和钟岚珠的单人卡池模拟')
+        await bot.finish(ev, '暂不支持米娅和钟岚珠的单人卡池模拟')  #这俩没ur 我就先写了这个 后续可以删除
         return
         
-    time_lmt.increase(uid)
-    up_num = get_group_config("up_num") 
-    gacha_cost = get_group_config("gacha_cost")
+    #time_lmt.increase(uid)  #删漏了 我靠
+    up_num = get_group_config("up_num")   #获取当前几张up卡
+    gacha_cost = get_group_config("gacha_cost") #获取预设的单抽耗费 
         
-    if str.strip(ev['match'].group(2)) == 'ur' or str.strip(ev['match'].group(2)) == 'UR':
+    if str.strip(ev['match'].group(2)) == 'ur' or str.strip(ev['match'].group(2)) == 'UR': #匹配当前是ur必得模式还是普通模式
         ur_mode = 1
-        
     else:
         ur_mode = 0
     
-    cost = gacha_cost if ur_mode == 0 else (gacha_cost * 10)
+    cost = gacha_cost if ur_mode == 0 else (gacha_cost * 10) #暂定ur必得模式10倍消耗
     
-    if ur_mode == 1:
-        if now_starstone < cost:
+    if ur_mode == 1: 
+        if now_starstone < cost:  #判断你钱够不够  
             await bot.send(ev, f'当前剩余星星{now_starstone},不足{cost}星星！')
             return
         else:
-            card_level = gacha_pup(up_num)
-            reduce_user_money(uid, key, cost)
+            card_level = gacha_pup(up_num) #这个在asgacha.py中本来是计划用于判断抽出ur是否是upur的 突然想到可以直接拿来作为ur必得的随卡函数用
+            reduce_user_money(uid, key, cost) #扣款 因为内部没写钱不够的校验 所有如果你需要调用请先进行校验再调用
     else:
-        if now_starstone < cost:
+        if now_starstone < cost:  #判断你钱够不够
             await bot.send(ev, f'当前剩余星星{now_starstone},不足{cost}星星！')
             return
         else:
-            card_level = gacha_pt(up_num)
+            card_level = gacha_pt(up_num) #随机获取一个卡片稀有度 1：r  2：sr 3：ur 4：pickup_ur1 5：pickup_ur2 up_num控制会不会返回5
             reduce_user_money(uid, key, cost)
 
-    _path, cid = random_give_card(card_level, member, team)
-    card_temp = db.add_card(uid, cid)
-    if card_temp > 6:
-        kirastone_num = 1 if card_level == 1 else (5 if card_level == 2 else 25)
-        increase_user_money(uid, "kirastone", kirastone_num)
+    _path, cid = random_give_card(card_level, member, team) #根据卡片稀有度 角色编号 团队编号直接给卡（顺序有点乱，主要是这个是第一版就写了的，那时候没想那么多
+    card_temp = db.add_card(uid, cid)  #将cid和uid存入db中，返回这卡你有几张了 （添加前有6张会返回7，但是不会加入，db里面存的还是6）
+    if card_temp > 6: #满突的话会添加失败，这里进行羽毛石获取的转换
+        kirastone_num = 1 if card_level == 1 else (5 if card_level == 2 else 25) #根据稀有度给羽毛石
+        increase_user_money(uid, "kirastone", kirastone_num) #增加货币量
 
     base = Image.open(base_path)  #加载底图
     f = get_pic(_path, 80)           #加载icon
     base.paste(f, (275,100), f)     #制图
-    if card_temp == 1:
+    if card_temp == 1:   #根据突数贴一些特殊图标，根据图标名字应该能看出来各个图标的用途
         f2 = get_pic("image/new.png", 80)                
         base.paste(f2, (275,100), f2)
     elif card_temp > 6:
@@ -243,7 +231,7 @@ async def gacha_one(bot, ev):  #单抽
 获得了{kirastone_num}羽毛石'''
     await bot.send(ev, msg, at_sender=True)
 
-@sv.on_rex((r'^(as|as )(.*)(十|10)(发|抽|连|连抽)(.*)'))                  #我讨厌正则表达式
+@sv.on_rex((r'^(as|as )(.*)(十|10)(发|抽|连|连抽)(.*)'))                  #我讨厌正则表达式  #后面的注释下次再补充
 async def gacha_ten(bot, ev):  #十连
     uid = ev['user_id']
     up_num = get_group_config("up_num") 
