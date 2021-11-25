32f649cc37e6f2aef1ed6b8e26c648934b850c96
kosakarin <91523573+kosakarin@users.noreply.github.com>
2021-11-25 14:44:59 +0800

    升级支持加成控分

--- countpoint.py
+++ countpoint.py
@@ -1,36 +1,4 @@
-
-#默认
-songs = [
-    875, 640, 600, 438, 405, 275,  247,  #S
-    860, 620, 581, 422, 390, 262,  236   #A
-]
-songs_list = [
-    '上+ S', '16LP剧情关 S', '上级 S', '13LP剧情关 S', '中级 S', '初级 S', '9LP剧情关 S',
-    '上+ A', '16LP剧情关 A', '上级 A', '13LP剧情关 A', '中级 A', '初级 A', '9LP剧情关 A'
-]
-#无剧情关
-songs_normal = [
-    875, 600, 405, 275,  #S
-    860, 581, 390, 262   #A
-]
-songs_list_normal = [
-    '上+ S', '上级 S', '中级 S', '初级 S',
-    '上+ A', '上级 A', '中级 A', '初级 A'
-]
-#无A
-songs_S = [
-    875, 640, 600, 438, 405, 275,  247  #S
-]
-songs_list_S = [
-    '上+ S', '16LP剧情关 S', '上级 S', '13LP剧情关 S', '中级 S', '初级 S', '9LP剧情关 S'
-]
-#无剧情关且全S
-songs_normal_S = [
-    875, 600, 405, 275
-]
-songs_list_normal_S = [
-    '上+ S', '上级 S', '中级 S', '初级 S'
-]
+import json,os
 
 def solve(songs, pt:int):
     #print(pt)
@@ -56,39 +24,68 @@ def solve(songs, pt:int):
     return mp[pt]
 
 def chose_list(mod:int):
+    with open(os.path.join(os.path.dirname(__file__), 'points.json'), "r", encoding='utf8') as f: 
+        points = json.load(f)   
+    _list = {"point":[],"name":[]}
     if mod == 1:
-        return songs_S, songs_list_S, '当前为仅S模式'
+        for i in points:
+            if i["rank"] == 'S':
+                _list["point"].append(i["point"])
+                _list["name"].append(i["name"] + i["rank"])
+        string = '当前为仅S模式'
     elif mod == 2:
-        return songs_normal, songs_list_normal, '当前为无剧情关卡模式'
+        for i in points:
+            if i["lpcost"] in [10,12,15,20]:
+                _list["point"].append(i["point"])
+                _list["name"].append(i["name"] + i["rank"])
+        string =  '当前为无剧情关卡模式'
     elif mod == 3:
-        return songs_normal_S, songs_list_normal_S, '当前为无剧情关卡仅S模式'
+        for i in points:
+            if i["lpcost"] in [10,12,15,20] and i["rank"] == 'S':
+                _list["point"].append(i["point"])
+                _list["name"].append(i["name"] + i["rank"])
+        string ='当前为无剧情关卡仅S模式'
     else:
-        return songs, songs_list, '未提供模式参数或模式参数识别失败，使用默认模式'
+        for i in points:
+            _list["point"].append(i["point"])
+            _list["name"].append(i["name"] + i["rank"])
+        string ='未提供模式参数或模式参数识别失败，使用默认模式'
 
-def tran(_songs, _songs_list, i:int) -> str:
+    return _list, string
+
+def tran(__song, _list, i):
     temp = 0
-    for song in _songs:
-        if i == song:
-            return _songs_list[temp]
+    for a in __song:
+        if a == i:
+            return _list["name"][temp]
         temp += 1
 
-def count_point(target:int, x:int, mod) -> str:
+def count_point(target:int, x:int, mod:int, _up:int) -> str:
     target -= x
     p = target
     cnt = 0
     msg = ''
-    _songs, _songs_list, mod_str = chose_list(mod)
+    __song = []
+    res_list = []
+    _list, mod_str = chose_list(mod)
+    for i in range(0, len(_list["point"])):
+        __song.append(int(_list["point"][i] * (_up / 100 + 1)))
     if target >= 50000:
-        cnt = (target - 49000)//875
-        target -= cnt * 875
-    res = solve(_songs, target)
-    msg += f'还需要获得{str(p)}pt,我们采用以下方案：\n注:以下结果为0%加成结果\n{mod_str}\n'
+        cnt = (target - 49000)//__song[0]
+        target -= cnt * __song[0]
+    res = solve(__song, target)
+    msg += f'还需要获得{str(p)}pt,我们采用以下方案：\n注:以下结果为{_up}%加成结果\n{mod_str}\n使用币值为：{__song}\n'
     if len(res) == 0:
         msg += '无解'
         return msg
     for i in res:
-        if i == 875:
+        if i == __song[0]:
             res[i] += cnt
-        msg += f'\n游玩{res[i]}次{tran(_songs, _songs_list, i)}，每次获得{str(i)}pt，共获得{str(i * res[i])}pt'
-  
+        msg += f'\n游玩{res[i]}次{tran(__song, _list, i)}，每次获得{str(i)}pt，共获得{str(i * res[i])}pt'
+        res_list.append(i * res[i])
+    temp = 0
+    for i in res_list:
+        temp += i
+    msg += f'\n反算获得pt为：{temp}'
+
     return msg
